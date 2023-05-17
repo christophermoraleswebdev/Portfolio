cd812fc2c382b2daf7906e519797731d3b88fdb1
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-05-16 21:27:01 -0500

    added hamburger menu and functionality to the menu

---
+++ script.js
@@ -0,0 +1,10 @@
+// Hamburger Menu 
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', function() {
+  menu.classList.toggle('menu-open');
+});
+
+
+
